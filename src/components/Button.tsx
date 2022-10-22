52547062439497a80012e4791eea815b49737073
shyhhh <hhhtheshy@gmail.com>
2022-10-22 21:40:58 +0800

    创建单独的 Button 组件

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #41454c;
+  border-radius: 4px;
+  color: #c4c7cf;
+`;
+export { Button };
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,15 +1,44 @@
+import { Button } from "components/Button";
+import Icon from "components/Icon";
+import Layout from "components/Layout";
 import React from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import { useTags } from "useTags";
 
 type Params = {
   id: string;
 };
+const Topbar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background: white;
+`;
 const Tag: React.FC = (props) => {
   const { findTag } = useTags();
   const { id } = useParams<Params>();
-  const tag = findTag(parseInt(id))
-  return <div>{tag.name}</div>;
+  const tag = findTag(parseInt(id));
+  return (
+    <Layout>
+      <Topbar>
+        <Icon name="left" />
+        <span>编辑标签</span>
+        <Icon />
+      </Topbar>
+      <div>
+        <label>
+          <span>标签名</span>
+          <input type="text" placeholder="标签名" />
+        </label>
+      </div>
+      <div>
+        <Button>删除标签</Button>
+      </div>
+    </Layout>
+  );
 };
 
 export { Tag };
